dc1588bc4f870eda7a89a227095df87736a2c861
fishfan <fishfan@tencent.com>
2021-11-18 14:08:11 +0800

    初始化一个项目地址

--- workflow/webpack.prod.config.js
+++
@@ -1,8 +0,0 @@
-const { merge } = require('webpack-merge');
-const commonConfig = require('./webpack.base.config');
-
-const devConfig = {
-  mode: 'production',
-  devtool: 'cheap-module-source-map',
-};
-module.exports = merge(commonConfig, devConfig);
