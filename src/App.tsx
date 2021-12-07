ebf5f07e60960295ae324ed883620c7c21fee11d
fishfan <fishfan@tencent.com>
2021-12-07 17:21:49 +0800

    首次提交

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,105 @@
-import React from 'react'
-import Banner from './imgs/fish.jpeg'
+import React,{useState} from 'react'
+import LoginImg from './img/log.svg'
+import Register from './img/register.svg'
 import './App.less'
 const Index = () => {
-  return <div className="app">
-    <img src={Banner} />
-    <h3>欢迎使用fish-cli</h3>
+  const [type,setType] = useState('')
+  return <div className={`container ${type==='signUp'?'sign-up-mode':''}` }>
+  <div className="forms-container">
+    <div className="signin-signup">
+      <form action="#" className="sign-in-form">
+        <h2 className="title">Sign in</h2>
+        <div className="input-field">
+          <i className="fas fa-user"></i>
+          <input type="text" placeholder="Username" />
+        </div>
+        <div className="input-field">
+          <i className="fas fa-lock"></i>
+          <input type="password" placeholder="Password" />
+        </div>
+        <input type="submit" value="Login" className="btn solid" />
+        <p className="social-text">Or Sign in with social platforms</p>
+        <div className="social-media">
+          <a href="#" className="social-icon">
+            <i className="fab fa-facebook-f"></i>
+          </a>
+          <a href="#" className="social-icon">
+            <i className="fab fa-twitter"></i>
+          </a>
+          <a href="#" className="social-icon">
+            <i className="fab fa-google"></i>
+          </a>
+          <a href="#" className="social-icon">
+            <i className="fab fa-linkedin-in"></i>
+          </a>
+        </div>
+      </form>
+      <form action="#" className="sign-up-form">
+        <h2 className="title">Sign up</h2>
+        <div className="input-field">
+          <i className="fas fa-user"></i>
+          <input type="text" placeholder="Username" />
+        </div>
+        <div className="input-field">
+          <i className="fas fa-envelope"></i>
+          <input type="email" placeholder="Email" />
+        </div>
+        <div className="input-field">
+          <i className="fas fa-lock"></i>
+          <input type="password" placeholder="Password" />
+        </div>
+        <input type="submit" className="btn" value="Sign up" />
+        <p className="social-text">Or Sign up with social platforms</p>
+        <div className="social-media">
+          <a href="#" className="social-icon">
+            <i className="fab fa-facebook-f"></i>
+          </a>
+          <a href="#" className="social-icon">
+            <i className="fab fa-twitter"></i>
+          </a>
+          <a href="#" className="social-icon">
+            <i className="fab fa-google"></i>
+          </a>
+          <a href="#" className="social-icon">
+            <i className="fab fa-linkedin-in"></i>
+          </a>
+        </div>
+      </form>
+    </div>
   </div>
+
+  <div className="panels-container">
+    <div className="panel left-panel">
+      <div className="content">
+        <h3>New here ?</h3>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis,
+          ex ratione. Aliquid!
+        </p>
+          <button onClick={() => {
+               setType('signUp')
+        }} className="btn transparent" id="sign-up-btn">
+          Sign up
+        </button>
+      </div>
+      <img src={LoginImg} className="image" alt="" />
+    </div>
+    <div className="panel right-panel">
+      <div className="content">
+        <h3>One of us ?</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+          laboriosam ad deleniti.
+        </p>
+          <button onClick={() => {
+            setType('signIn')
+        }} className="btn transparent" id="sign-in-btn">
+          Sign in
+        </button>
+      </div>
+      <img src={Register} className="image" alt="" />
+    </div>
+  </div>
+</div>
 }
 export default Index;
